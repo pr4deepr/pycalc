4eaedffba4af36185fa705d1db6dbb676911a0a0
Pradeep <pradeep.rajasekhar@gmail.com>
2020-05-30 04:28:27 +1000

    Fixed doc to match PEP257 convention

--- calc.py
+++ calc.py
@@ -1,5 +1,7 @@
-# Perform simple arithmetic encoded in an input string:
-# '1 + 2' -> 3, or '1 - 2' -> -1.
+"""Perform simple arithmetic encoded in an input string:
+
+'1 + 2' -> 3, or '1 - 2' -> -1."""
+
 def compute(expression):
     num0, operator, num1= expression.split(' ')
     num0,num1=int(num0),int(num1)
